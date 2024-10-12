b122a3ddfb73b6000f4dc339535c7341e86a5c2e
mfyx <1571547820@qq.com>
2024-10-12 23:45:01 +0800

    set num_workers to 8 to adapt my system

--- code/data/__init__.py
+++ code/data/__init__.py
@@ -16,8 +16,11 @@ def create_dataloader(dataset, dataset_opt, opt=None, sampler=None):
             num_workers = dataset_opt['n_workers'] * len(opt['gpu_ids'])
             batch_size = dataset_opt['batch_size']
             shuffle = True
+        #return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+        #                                   num_workers=num_workers, sampler=sampler, drop_last=True,
+        #                                   pin_memory=False)
         return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
-                                           num_workers=num_workers, sampler=sampler, drop_last=True,
+                                           num_workers=8, sampler=sampler, drop_last=True,
                                            pin_memory=False)
     else:
         return torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1,
